711e7457d178858ac69ac9c02a283e8191a076bf
abollu779 <abollu779@gmail.com>
2020-06-04 12:17:12 -0400

    Chunking data only for separatehiddens where necessary

--- predict_brain_from_nlp.py
+++ predict_brain_from_nlp.py
@@ -40,32 +40,37 @@ if __name__ == '__main__':
 
     # loading fMRI data
     data = np.load('./data/fMRI/data_subject_{}.npy'.format(args.subject))
-    # # limit to ROI data
-    #     if args.use_roi_voxels:
-    #         rois = np.load('../HP_subj_roi_inds.npy', allow_pickle=True)
-    #         data = data[:, np.where(rois.item()[args.subject]['all'] == 1)[0]]
-    num_voxels = data.shape[1]
-    chunk_size = num_voxels//5
+    # limit to ROI data
+    if args.use_roi_voxels:
+        rois = np.load('../HP_subj_roi_inds.npy', allow_pickle=True)
+        data = data[:, np.where(rois.item()[args.subject]['all'] == 1)[0]]
 
-    corrs_t, preds_t, test_t, train_losses_t, test_losses_t = [], [], [], [], []
+    if args.encoding_model == 'mlp_allvoxels_separatehidden':
+        num_voxels = data.shape[1]
+        chunk_size = num_voxels//5
 
-    for data_start in range(0, num_voxels, chunk_size):
-        data_chunk = data[:, data_start:data_start+chunk_size]
+        corrs_t, preds_t, test_t, train_losses_t, test_losses_t = [], [], [], [], []
 
-        corrs, preds, test, train_losses, test_losses = run_class_time_CV_fmri_crossval_ridge(data_chunk,
-                                                                    predict_feat_dict)
+        for data_start in range(0, num_voxels, chunk_size):
+            data_chunk = data[:, data_start:data_start+chunk_size]
+
+            corrs, preds, test, train_losses, test_losses = run_class_time_CV_fmri_crossval_ridge(data_chunk,
+                                                                            predict_feat_dict)
+            
+            corrs_t.append(corrs)
+            preds_t.append(preds)
+            test_t.append(test)
+            train_losses_t.append(train_losses)
+            test_losses_t.append(test_losses)
         
-        corrs_t.append(corrs)
-        preds_t.append(preds)
-        test_t.append(test)
-        train_losses_t.append(train_losses)
-        test_losses_t.append(test_losses)
-    
-    corrs_t = np.concatenate(corrs_t, axis=0)
-    preds_t = np.concatenate(preds_t, axis=1)
-    test_t = np.concatenate(test_t, axis=1)
-    train_losses_t = np.stack(train_losses_t, axis=0)
-    test_losses_t = np.stack(test_losses_t, axis=0)
+        corrs_t = np.concatenate(corrs_t, axis=0)
+        preds_t = np.concatenate(preds_t, axis=1)
+        test_t = np.concatenate(test_t, axis=1)
+        train_losses_t = np.stack(train_losses_t, axis=0)
+        test_losses_t = np.stack(test_losses_t, axis=0)
+    else:
+        corrs_t, preds_t, test_t, train_losses_t, test_losses_t = run_class_time_CV_fmri_crossval_ridge(data,
+                                                                            predict_feat_dict)
 
 
     if not args.single_fold_computation:
--- utils/mlp_encoding_utils.py
+++ utils/mlp_encoding_utils.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import numpy as np
 
 class MLPEncodingModel(nn.Module):
-    def __init__(self, input_size, hidden_sizes, output_size, is_mlp_allvoxels = False):
+    def __init__(self, input_size, hidden_sizes, output_size, is_mlp_allvoxels_separatehidden = False):
         super(MLPEncodingModel, self).__init__()
         self.model = None
         if len(hidden_sizes) == 2:
@@ -16,7 +16,7 @@ class MLPEncodingModel(nn.Module):
             self.model = nn.Sequential(nn.Linear(input_size, hidden_sizes[0]), 
                         nn.ReLU(), 
                         nn.Linear(hidden_sizes[0], output_size))
-            if is_mlp_allvoxels:
+            if is_mlp_allvoxels_separatehidden:
                 num_voxels = self.model[2].weight.shape[0]
                 linear2_weights = self.model[2].weight.detach().numpy() 
                 # Need to convert to numpy and adjust that array.
