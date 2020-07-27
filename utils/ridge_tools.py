8c866073a6e2b730d4915764644b079e6df7b612
abollu779 <abollu779@gmail.com>
2020-07-27 19:09:26 -0400

    Resolved minor bug with printing train loss

--- utils/ridge_tools.py
+++ utils/ridge_tools.py
@@ -560,7 +560,6 @@ def ridge_by_lambda_grad_descent(model_dict, X, Y, Xval, Yval, lambdas, lrs, spl
             preds_val = model(Xval)
             val_loss = 1 - R2_torch(preds_val.squeeze(), Yval)
             overall_val_loss = criterion(preds_val.squeeze(), Yval)
-            train_loss = criterion(model(X).squeeze(), Y)
 
             epoch_loss /= num_batches
             epoch_losses[idx, epoch] = epoch_loss
@@ -590,11 +589,11 @@ def ridge_by_lambda_grad_descent(model_dict, X, Y, Xval, Yval, lambdas, lrs, spl
             # if new_lr != prev_lr:
             #     print("Epoch: {}, LR: {}".format(epoch, new_lr))
 
-            if (epoch%5 == 0) or (epoch == curr_n_epochs-1):
+            if (epoch%10 == 0) or (epoch == curr_n_epochs-1):
             # if (epoch == 0) or (epoch == curr_n_epochs-1):
                 # print("Lambda: {}, Epoch: {}".format(lmbda, epoch))
                 # print("Grad Norms: {}".format(torch.abs(model.model[0].weight.grad)))
-                print("Epoch: {}, GradNorm: {}, TLoss: {}, VLoss: {}".format(epoch, sum_grad_norm, train_loss, overall_val_loss.detach().cpu().item()))
+                print("Epoch: {}, GradNorm: {}, TLoss: {}, VLoss: {}".format(epoch, sum_grad_norm, epoch_loss, overall_val_loss.detach().cpu().item()))
             #     # if epoch == curr_n_epochs-1:
             #     #     import pdb
             #     #     pdb.set_trace()
